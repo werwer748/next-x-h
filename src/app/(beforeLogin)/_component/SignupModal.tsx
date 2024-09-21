6b78d0cdd00538a9a28738fff74285399def1d00
hugo <werwer748@naver.com>
2024-09-21 17:11:05 +0900

    서버 컴포넌트에서 액션!

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,99 +1,114 @@
-"use client"; // hook을 써야하면 무조건 클라이언트 컴포넌트화!
-
+//* 정식 기능이 된 서버 액션을 사용하기 위해 서버 컴포넌트로 변경
 import style from "@/app/(beforeLogin)/_component/signup.module.css";
-import { useRouter} from "next/navigation";
-import {ChangeEventHandler, FormEvent, FormEventHandler, useState} from "react";
+import BackButton from "@/app/(beforeLogin)/_component/BackButton";
+import {redirect} from "next/navigation";
 
 export default function SignupModal() {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [image, setImage] = useState('');
-  const [imageFile, setImageFile] = useState<File>();
 
-  const router = useRouter();
-
-  const onClickClose = () => {
-    router.back();
-    // TODO: 뒤로가기가 /home이 아니면 /home으로 보내기???
-  };
-
-  const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => { setId(e.target.value) };
-  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => { setPassword(e.target.value) };
-  const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (e) => { setNickname(e.target.value) };
-  const onChangeImageFile: ChangeEventHandler<HTMLInputElement> = (e) => {
-    e.target.files && setImageFile(e.target.files[0]);
-  };
-
-  const onSubmit: FormEventHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    fetch('http://localhost:9090/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        id,
-        nickname,
-        image,
-        password,
-      }),
-      credentials: 'include',
-    }).then((response: Response) => {
+  const submit = async (formData: FormData) => {
+    /**
+     * * use server
+     * ? 서버 액션으로 사용할 수 있게 해준다.
+     * ? 브라우저에 노출되지 않는 코드기 때문에 안전하게 키값 사용이 가능함
+     */
+    "use server";
+    
+    //* formData validation
+    if (!formData.get('id')) {
+      return {message: 'no_id' }
+    }
+    if (!formData.get('name')) {
+      return {message: 'no_name' };
+    }
+    if (!formData.get('password')) {
+      return {message: 'no_password' };
+    }
+    if (!formData.get('image')) {
+      return {message: 'no_image' };
+    }
+    
+    // next/navigation의 redirect는 try/catch에서 쓸 수 없기 때문에 이렇게 처리
+    let goRedirect = false;
+    
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`, {
+        method: "POST",
+        body: formData,
+        credentials: 'include' // 쿠키 전달을 위해 추가
+      });
       console.log(response.status);
-      if (response.status === 200) {
-        router.replace('/home');
+      if (response.status === 403) {
+        return { message: 'user_exists' }
       }
-    }).catch((err) => {
-      console.error(err);
-    });
+      console.log(await response.json());
+      goRedirect = true;
+    } catch (error) {
+      console.error(error);
+    }
+    
+    if (goRedirect) {
+      redirect('/home'); // try/catch문에서 절대로 사용해서는 안됨
+    }
   }
-
+  
   return (
     <>
       <div className={style.modalBackground}>
         <div className={style.modal}>
           <div className={style.modalHeader}>
-            <button className={style.closeButton} onClick={onClickClose}>
-              <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
-                   className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
-                <g>
-                  <path
-                          d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-                </g>
-              </svg>
-            </button>
+            <BackButton/>
             <div>계정을 생성하세요.</div>
           </div>
-          <form>
+          {/* action의 submit이 서버액션 */}
+          <form action={submit}>
             <div className={style.modalBody}>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="id">아이디</label>
-                <input id="id" className={style.input} type="text" placeholder=""
-                       value={id}
-                       onChange={onChangeId}
+                <input
+                  className={style.input}
+                  id="id"
+                  name={"id"} // form을 통쨰로 받아서써야하기 때문에 name 추가
+                  type="text"
+                  placeholder=""
+                  required // validation을 input에서 직접 처리해야 함.
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="name">닉네임</label>
-                <input id="name" className={style.input} type="text" placeholder=""
-                       value={nickname}
-                       onChange={onChangeNickname}
+                <input
+                  id="name"
+                  name={"name"}
+                  className={style.input}
+                  type="text"
+                  placeholder=""
+                  required
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="password">비밀번호</label>
-                <input id="password" className={style.input} type="password" placeholder=""
-                       value={password}
-                       onChange={onChangePassword}
+                <input
+                  id="password"
+                  name="password"
+                  className={style.input}
+                  type="password"
+                  placeholder=""
+                  required
                 />
               </div>
               <div className={style.inputDiv}>
                 <label className={style.inputLabel} htmlFor="image">프로필</label>
-                <input id="image" className={style.input} type="file" accept="image/*"
-                       onChange={onChangeImageFile}
+                <input
+                  id="image"
+                  name="image"
+                  className={style.input}
+                  type="file"
+                  accept="image/*"
+                  required
                 />
               </div>
             </div>
             <div className={style.modalFooter}>
-              <button className={style.actionButton} disabled>가입하기</button>
+              <button type={"submit"} className={style.actionButton}>가입하기</button>
             </div>
           </form>
         </div>
